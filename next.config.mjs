9d1b04e875142022ae31298ae55525d2a1d6fe1d
Bhanu1776 <sunkabhanu281202@gmail.com>
2024-06-04 09:16:31 +0530

    style(App): Changed accent to green and added million lint to the project

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,14 @@
+import MillionLint from '@million/lint';
+import million from 'million/compiler';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
     dirs: ['src'],
-    ignoreDuringBuilds: true,
-  },
+    ignoreDuringBuilds: true
+  }
 };
-
-export default nextConfig;
+export default million.next(MillionLint.next({
+  rsc: true
+})(nextConfig), {
+  auto: true
+});
