32ce55a2ee7ff59bcd4fdfa2f94f3f038b8ccd17
Bhanu1776 <sunkabhanu281202@gmail.com>
2024-06-04 19:01:08 +0530

    feat(App): Added Navbar with fully authenication using kinde
    - This is the way to setup kind in Nextjs Application, better to donwload the starter kit of Kinde and copy the api folder from their repo
    - Removed Million lint temporarily

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,15 @@
-import MillionLint from '@million/lint';
-import million from 'million/compiler';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
     dirs: ['src'],
-    ignoreDuringBuilds: true
-  }
+    ignoreDuringBuilds: true,
+  },
 };
-export default million.next(MillionLint.next({
-  rsc: true
-})(nextConfig), {
-  auto: true
-});
+// export default million.next(
+//   MillionLint.next({
+//     rsc: true,
+//   })(nextConfig),
+//   {
+//     auto: true,
+//   },
+// );
