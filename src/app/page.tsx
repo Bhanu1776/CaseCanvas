4ca16aed2d6ee59602887f7a92f1dbb99ed0a699
Bhanu1776 <sunkabhanu281202@gmail.com>
2024-06-05 23:36:33 +0530

    feat(Home): Add Examples Section using framer motion
    The most difficult component to design and implement :)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import { Examples } from '@/app/(Home)/(Examples)/page';
 import HeroSection from '@/app/(Home)/(Hero)/page';
+import Testimonials from '@/app/(Home)/(Testimonials)/page';
 
 export default function Home() {
   return (
     <div className="bg-slate-50">
       <HeroSection />
+      <Testimonials />
+      <Examples />
     </div>
   );
 }
