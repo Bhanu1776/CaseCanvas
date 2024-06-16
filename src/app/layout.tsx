46528071810fcb6026e80133b4b1b1b6bb2b70a5
Bhanu1776 <sunkabhanu281202@gmail.com>
2024-06-16 12:28:15 +0530

    chore: Smartest way to structure the layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import Footer from '@/components/Navigation/Footer';
 import Navbar from '@/components/Navigation/Navbar';
+import { Toaster } from '@/components/ui/toaster';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
@@ -20,8 +21,15 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Navbar />
-        {children}
-        <Footer />
+
+        {/* //* Very Important Trick -> Pushing the footer to the end of the screen and the space below footer will be free */}
+        <main className="grainy-light flex min-h-[calc(100vh-3.5rem-1px)] flex-col">
+          <div className="flex h-full flex-1 flex-col">{children}</div>
+          {/* //* Flex-1 -> Will take the entire space between the Navbar and Footer */}
+          <Footer />
+        </main>
+
+        <Toaster />
       </body>
     </html>
   );
