fb94b47b7daf9431648dd4124809cef8bd559df0
Bhanu1776 <sunkabhanu281202@gmail.com>
2024-06-23 14:39:02 +0530

    feat: Implemented step 2 of configurator page

---
+++ src/app/configure/design/components/HandleComponent.tsx
@@ -0,0 +1,8 @@
+//* Handle component is nothing but a, resizing circular icon for resizing the image in the step 2 of the configurator page
+const HandleComponent = () => {
+  return (
+    <div className="size-5 rounded-full border border-zinc-200 bg-white shadow transition hover:bg-primary" />
+  );
+};
+
+export default HandleComponent;
