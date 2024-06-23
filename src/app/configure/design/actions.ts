c79e584b32a958be4de4fdfcf957ffbcc498322a
Bhanu1776 <sunkabhanu281202@gmail.com>
2024-06-23 14:41:58 +0530

    chore: Integrated Server actions with react query to update database with prisma schema

---
+++ src/app/configure/design/actions.ts
@@ -0,0 +1,31 @@
+'use server';
+
+import { db } from '@/db';
+import {
+  CaseColor,
+  CaseFinish,
+  CaseMaterial,
+  PhoneModel,
+} from '@prisma/client';
+
+export type SaveConfigArgs = {
+  color: CaseColor;
+  finish: CaseFinish;
+  material: CaseMaterial;
+  model: PhoneModel;
+  configId: string;
+};
+
+//* This is something we call it as RPC (Remote procedure call)
+export async function saveConfig({
+  color,
+  finish,
+  material,
+  model,
+  configId,
+}: SaveConfigArgs) {
+  await db.configuration.update({
+    where: { id: configId },
+    data: { color, finish, material, model },
+  });
+}
