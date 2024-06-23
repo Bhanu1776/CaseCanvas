8680d233560439a689c17303a8ed245b43162ee0
Bhanu1776 <sunkabhanu281202@gmail.com>
2024-06-23 14:32:54 +0530

    build: Add tanstack react query

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import Footer from '@/components/Navigation/Footer';
 import Navbar from '@/components/Navigation/Navbar';
+import Providers from '@/components/Providers';
 import { Toaster } from '@/components/ui/toaster';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
@@ -24,7 +25,9 @@ export default function RootLayout({
 
         {/* //* Very Important Trick -> Pushing the footer to the end of the screen and the space below footer will be free */}
         <main className="grainy-light flex min-h-[calc(100vh-3.5rem-1px)] flex-col">
-          <div className="flex h-full flex-1 flex-col">{children}</div>
+          <div className="flex h-full flex-1 flex-col">
+            <Providers>{children}</Providers>
+          </div>
           {/* //* Flex-1 -> Will take the entire space between the Navbar and Footer */}
           <Footer />
         </main>
