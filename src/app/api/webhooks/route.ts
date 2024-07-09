67c8ed1d15fbbbddcd7e262d815b4a3bdc72ea3d
Bhanu1776 <sunkabhanu281202@gmail.com>
2024-07-09 22:23:25 +0530

    test: Resend email 403 error

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -77,7 +77,7 @@ export async function POST(req: Request) {
       });
 
       await resend.emails.send({
-        from: 'caseCanvas <sunkabhanu281202@gmail.com>',
+        from: 'sunkabhanu281202@gmail.com',
         to: [event.data.object.customer_details.email],
         subject: 'Thanks for your order!',
         react: OrderReceivedEmail({
@@ -94,6 +94,7 @@ export async function POST(req: Request) {
           },
         }),
       });
+      console.log(`Email sent to ${event.data.object.customer_details.email}`);
     }
 
     return NextResponse.json({ result: event, ok: true });
