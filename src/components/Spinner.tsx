8cb10c791e10963bc886c7587916e26ce952ba12
Bhanu1776 <sunkabhanu281202@gmail.com>
2024-07-12 14:03:47 +0530

    feat: Add nice spinner for loading animation through out the app

---
+++ src/components/Spinner.tsx
@@ -0,0 +1,16 @@
+const LoadingComponent = () => {
+  return (
+    <div className="spinner-overlay">
+      <div className="spinner">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default LoadingComponent;
